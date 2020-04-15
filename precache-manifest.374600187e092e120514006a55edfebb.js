3a5816fd3091fe5e5436d7ead93a627f8fd007d9
tokyo-cement-gp <mathuthiyasharma15@gmail.com>
2020-04-15 19:52:13 +0530

    Updates

---
+++ precache-manifest.374600187e092e120514006a55edfebb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d12ce468a799ce94bc6aa73b016a1fb",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "eced41485c1d2980d06b",
+    "url": "/deploy-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7fe37470284fb9f4a28e",
+    "url": "/deploy-react/static/js/2.a56eee3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.a56eee3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eced41485c1d2980d06b",
+    "url": "/deploy-react/static/js/main.af8e7b07.chunk.js"
+  },
+  {
+    "revision": "3fc45ca82b10a6fdda25",
+    "url": "/deploy-react/static/js/runtime-main.ee59e27b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
